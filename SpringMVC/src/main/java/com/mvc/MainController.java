fe3660cb36428cf94fa0d90da0c709e5d178629d
elzenaro <nam2626@naver.com>
2024-06-13 11:10:03 +0900

    mvc

--- SpringMVC/src/main/java/com/mvc/MainController.java
+++ SpringMVC/src/main/java/com/mvc/MainController.java
@@ -1,8 +1,13 @@
 package com.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String loginView() {
+		return "login";
+	}
 }
