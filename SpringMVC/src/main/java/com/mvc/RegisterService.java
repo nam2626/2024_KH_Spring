66fd631ffaf9b2bc27b6bda6714003029dca0a3c
elzenaro <nam2626@naver.com>
2024-06-13 16:40:01 +0900

    mvc

---
+++ SpringMVC/src/main/java/com/mvc/RegisterService.java
@@ -0,0 +1,11 @@
+package com.mvc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService {
+
+	public void test() {
+		System.out.println("서비스 클래스 테스트용 메서드");
+	}
+}
