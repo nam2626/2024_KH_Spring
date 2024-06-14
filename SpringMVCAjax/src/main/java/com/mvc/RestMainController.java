96e1e7efd8dee6d3f7d96e6e951cb8cca4ba7817
elzenaro <nam2626@naver.com>
2024-06-14 10:48:57 +0900

    Ajax 테스트

--- SpringMVCAjax/src/main/java/com/mvc/RestMainController.java
+++ SpringMVCAjax/src/main/java/com/mvc/RestMainController.java
@@ -1,6 +1,10 @@
 package com.mvc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,31 @@ public class RestMainController {
 	public String main() {
 		return "index";
 	}
+	
+	//객체 리턴
+	@RequestMapping("/get")
+	public RegisterDTO getRegister() {
+		return new RegisterDTO("A0001", "123456", "홍길동", "남", 30);
+	}
+	
+	//맵 리턴
+	@RequestMapping("/map")
+	public HashMap<String, Object> returnMap(){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("msg", "Exception이 발생되지 않았습니다.");
+		map.put("resultCount", 30);
+		ArrayList<RegisterDTO> list = new ArrayList<RegisterDTO>();
+		list.add(new RegisterDTO("A0001", "123456", "홍길동", "남", 30));
+		list.add(new RegisterDTO("A0002", "123456", "홍길동", "남", 40));
+		list.add(new RegisterDTO("A0003", "123456", "홍길동", "남", 50));
+		list.add(new RegisterDTO("A0004", "123456", "홍길동", "남", 60));
+		map.put("list", list);
+		return map;		
+	}
 }
+
+
+
+
+
+
