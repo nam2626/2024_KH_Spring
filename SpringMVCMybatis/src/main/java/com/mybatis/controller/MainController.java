68ea0479cc88f84067e6eedaa2e7b5273554b24a
elzenaro <nam2626@naver.com>
2024-06-14 14:20:29 +0900

    Mybatis

--- SpringMVCMybatis/src/main/java/com/mybatis/controller/MainController.java
+++ SpringMVCMybatis/src/main/java/com/mybatis/controller/MainController.java
@@ -3,12 +3,30 @@ package com.mybatis.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mybatis.service.StudentService;
+
 @Controller
 public class MainController {
 	
+	private StudentService studentService;
+	
+	public MainController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
 	@GetMapping("/")
 	public String index() {
-		return "main.jsp";
+		return "main";
+	}
+	
+	@GetMapping("/addStdView")
+	public String addStudentView() {
+		return "student_register";
 	}
 	
 }
+
+
+
+
+
