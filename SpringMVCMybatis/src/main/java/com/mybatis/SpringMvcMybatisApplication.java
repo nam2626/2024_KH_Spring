8da663f8f58870b5e5ecbb7680e5fe0458db9a00
elzenaro <nam2626@naver.com>
2024-06-14 12:16:46 +0900

    Ajax 테스트

---
+++ SpringMVCMybatis/src/main/java/com/mybatis/SpringMvcMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMybatisApplication.class, args);
+	}
+
+}
