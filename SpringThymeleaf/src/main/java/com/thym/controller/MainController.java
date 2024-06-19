b48cfff7365547a143abd28d19a2da33f6708f7a
elzenaro <nam2626@naver.com>
2024-06-19 16:44:09 +0900

    Thymeleaf

--- SpringThymeleaf/src/main/java/com/thym/controller/MainController.java
+++ SpringThymeleaf/src/main/java/com/thym/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.thym.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.thym.dto.BoardMemberDTO;
@@ -136,6 +143,19 @@ public class MainController {
 		view.addObject("list", list);
 		view.setViewName("grade_main");
 		return view;
+	}
+	
+	@PutMapping("/grade/update")
+	public ResponseEntity<String> updateGrade(GradeDTO dto){
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		System.out.println(dto);
+
+		//수정 작업
+		
+		//수정 결과 메세지
+ 		map.put("result", "호출완료");
+		return new ResponseEntity(map, HttpStatus.OK);
 	}
 }
 
