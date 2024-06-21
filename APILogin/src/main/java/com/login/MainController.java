dfe25f1ea0415026416cb511f1e8d6c51c237b01
elzenaro <nam2626@naver.com>
2024-06-21 11:42:24 +0900

    APILogin

--- APILogin/src/main/java/com/login/MainController.java
+++ APILogin/src/main/java/com/login/MainController.java
@@ -19,7 +19,7 @@ import jakarta.servlet.http.HttpSession;
 public class MainController {
 	private final String CLIENT_ID = "";
 	private final String CLIENT_SECRET_ID = "";
-	
+
 	@GetMapping("/naver")
 	public ModelAndView naverLoginView(ModelAndView view, HttpSession session) throws UnsupportedEncodingException {
 		String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
@@ -34,55 +34,55 @@ public class MainController {
 		view.setViewName("naver_login");
 		return view;
 	}
-	
+
 	@GetMapping("/naver/callback")
-	public ModelAndView naverCallBack(HttpSession session, ModelAndView view, String state, String code) throws UnsupportedEncodingException {
+	public ModelAndView naverCallBack(HttpSession session, ModelAndView view, String state, String code)
+			throws UnsupportedEncodingException {
 		String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
-	    String apiURL;
-	    apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
-	    apiURL += "client_id=" + CLIENT_ID;
-	    apiURL += "&client_secret=" + CLIENT_SECRET_ID;
-	    apiURL += "&redirect_uri=" + redirectURI;
-	    apiURL += "&code=" + code;
-	    apiURL += "&state=" + state;
-	    String access_token = "";
-	    String refresh_token = "";
-	    System.out.println("apiURL="+apiURL);
-	    try {
-	      URL url = new URL(apiURL);
-	      HttpURLConnection con = (HttpURLConnection)url.openConnection();
-	      con.setRequestMethod("GET");
-	      int responseCode = con.getResponseCode();
-	      BufferedReader br;
-	      System.out.print("responseCode="+responseCode);
-	      if(responseCode==200) { // 정상 호출
-	        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-	      } else {  // 에러 발생
-	        br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-	      }
-	      String inputLine;
-	      StringBuffer res = new StringBuffer();
-	      while ((inputLine = br.readLine()) != null) {
-	        res.append(inputLine);
-	      }
-	      br.close();
-	      if(responseCode==200) {
-	    	  session.setAttribute("user", res.toString());
-	      }
-	    } catch (Exception e) {
-	      System.out.println(e);
-	    }
+		String apiURL;
+		apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+		apiURL += "client_id=" + CLIENT_ID;
+		apiURL += "&client_secret=" + CLIENT_SECRET_ID;
+		apiURL += "&redirect_uri=" + redirectURI;
+		apiURL += "&code=" + code;
+		apiURL += "&state=" + state;
+		String access_token = "";
+		String refresh_token = "";
+		System.out.println("apiURL=" + apiURL);
+		
 		view.setViewName("naver_login_result");
 		return view;
 	}
 
+	public String requestNaverServer(String apiURL, String header) {
+		StringBuffer res = new StringBuffer();
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			if (header != null && !header.equals("")) {
+				con.setRequestProperty("Authorization", header);
+			}
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			System.out.print("responseCode=" + responseCode);
+			if (responseCode == 200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else { // 에러 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			while ((inputLine = br.readLine()) != null) {
+				res.append(inputLine);
+			}
+			br.close();
+			if (responseCode == 200) {
+				System.out.println(res.toString());
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return res.toString();
+	}
 }
-
-
-
-
-
-
-
-
-
