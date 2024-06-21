6b15592fbfa10a3de66bc86aaf6ab6e1d97bd6f8
elzenaro <nam2626@naver.com>
2024-06-21 10:59:10 +0900

    APILogin

--- APILogin/src/main/java/com/login/MainController.java
+++ APILogin/src/main/java/com/login/MainController.java
@@ -1,7 +1,11 @@
 package com.login;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.security.SecureRandom;
 
@@ -13,8 +17,8 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
-	private final String CLIENT_ID = "CLIENT_ID";
-	private final String CLIENT_SECRET_ID = "CLIENT_SECRET_ID";
+	private final String CLIENT_ID = "";
+	private final String CLIENT_SECRET_ID = "";
 	
 	@GetMapping("/naver")
 	public ModelAndView naverLoginView(ModelAndView view, HttpSession session) throws UnsupportedEncodingException {
@@ -30,5 +34,55 @@ public class MainController {
 		view.setViewName("naver_login");
 		return view;
 	}
+	
+	@GetMapping("/naver/callback")
+	public ModelAndView naverCallBack(HttpSession session, ModelAndView view, String state, String code) {
+		String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
+	    String apiURL;
+	    apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+	    apiURL += "client_id=" + CLIENT_ID;
+	    apiURL += "&client_secret=" + CLIENT_SECRET_ID;
+	    apiURL += "&redirect_uri=" + redirectURI;
+	    apiURL += "&code=" + code;
+	    apiURL += "&state=" + state;
+	    String access_token = "";
+	    String refresh_token = "";
+	    System.out.println("apiURL="+apiURL);
+	    try {
+	      URL url = new URL(apiURL);
+	      HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	      con.setRequestMethod("GET");
+	      int responseCode = con.getResponseCode();
+	      BufferedReader br;
+	      System.out.print("responseCode="+responseCode);
+	      if(responseCode==200) { // 정상 호출
+	        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	      } else {  // 에러 발생
+	        br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	      }
+	      String inputLine;
+	      StringBuffer res = new StringBuffer();
+	      while ((inputLine = br.readLine()) != null) {
+	        res.append(inputLine);
+	      }
+	      br.close();
+	      if(responseCode==200) {
+	        out.println(res.toString());
+	      }
+	    } catch (Exception e) {
+	      System.out.println(e);
+	    }
+		
+		return view;
+	}
 
 }
+
+
+
+
+
+
+
+
+
