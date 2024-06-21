4217354373ea07a4cd76fcb7f288298af319421d
elzenaro <nam2626@naver.com>
2024-06-21 14:18:21 +0900

    APILogin

---
+++ APILogin/src/main/java/com/login/KakaoController.java
@@ -0,0 +1,14 @@
+package com.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class KakaoController {
+	private final String REST_API_KEY = "";
+	
+	//kakao_login.html
+	//kakao_login_result.html
+	
+	
+	
+}
