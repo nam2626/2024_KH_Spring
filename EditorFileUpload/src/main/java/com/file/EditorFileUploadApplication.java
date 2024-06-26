dfe79c03dbdc6f013dbe60bffe1b75a437459f83
elzenaro <nam2626@naver.com>
2024-06-26 14:03:47 +0900

    에디터 적용

---
+++ EditorFileUpload/src/main/java/com/file/EditorFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorFileUploadApplication.class, args);
+	}
+
+}
