de180b35f82c89caa872fa8fe993c796a095d4b0
elzenaro <nam2626@naver.com>
2024-06-26 14:16:37 +0900

    파일 업로드

--- EditorFileUpload/src/main/java/com/file/controller/MainController.java
+++ EditorFileUpload/src/main/java/com/file/controller/MainController.java
@@ -2,6 +2,9 @@ package com.file.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MainController {
@@ -15,4 +18,19 @@ public class MainController {
 		return "board_write";
 	}
 	
+	@PostMapping("/board/write")
+	public String boardWrite(String title, String content, String writer,
+			@RequestParam(value="file") MultipartFile[] file) {
+		for(int i=0;i<file.length;i++) {
+			System.out.println(file[i].getSize() + " " + file[i].getOriginalFilename());
+		}
+		
+		return null;
+	}
 }
+
+
+
+
+
+
