2669e25d429a039969c7d9f837aab1b62a36c613
elzenaro <nam2626@naver.com>
2024-06-26 14:21:13 +0900

    파일 업로드

--- EditorFileUpload/src/main/java/com/file/controller/MainController.java
+++ EditorFileUpload/src/main/java/com/file/controller/MainController.java
@@ -21,8 +21,13 @@ public class MainController {
 	@PostMapping("/board/write")
 	public String boardWrite(String title, String content, String writer,
 			@RequestParam(value="file") MultipartFile[] file) {
+		
+		//업로드할 기본 경로 지정 및 없으면 생성
+		
 		for(int i=0;i<file.length;i++) {
 			System.out.println(file[i].getSize() + " " + file[i].getOriginalFilename());
+			//파일 쓰기
+			
 		}
 		
 		return null;
