9fb3402e4a8ba48a09c0e6e9eaec45c5e898d571
elzenaro <nam2626@naver.com>
2024-06-26 14:47:42 +0900

    파일 업로드

--- EditorFileUpload/src/main/java/com/file/controller/MainController.java
+++ EditorFileUpload/src/main/java/com/file/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.file.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +23,22 @@ public class MainController {
 	
 	@PostMapping("/board/write")
 	public String boardWrite(String title, String content, String writer,
-			@RequestParam(value="file") MultipartFile[] file) {
+			@RequestParam(value="file") MultipartFile[] file) throws IllegalStateException, IOException {
 		
 		//업로드할 기본 경로 지정 및 없으면 생성
+		File root = new File("c:\\fileupload");
+		if(!root.exists())
+			root.mkdirs();
 		
 		for(int i=0;i<file.length;i++) {
 			System.out.println(file[i].getSize() + " " + file[i].getOriginalFilename());
+			//파일 사이즈 체크 해서 0이면 업로드가 안된 항목
+			if(file[i].getSize() == 0)
+				continue;
 			//파일 쓰기
+			//업로드할 경로 설정
+			File f = new File(root, file[i].getOriginalFilename());
+			file[i].transferTo(f);//실제 파일 쓰기를 수행
 			
 		}
 		
