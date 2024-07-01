386b11e42b61ac748bcae68e3747d63e08fa8704
elzenaro <nam2626@naver.com>
2024-07-01 11:10:01 +0900

    회원정보 수정

--- SpringBoardProject/src/main/java/com/board/controller/MainController.java
+++ SpringBoardProject/src/main/java/com/board/controller/MainController.java
@@ -3,9 +3,7 @@ package com.board.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,7 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -476,6 +474,18 @@ public class MainController {
 		Map<String, Object> map = new HashMap<String,Object>();
 		map.put("count", count);
 		map.put("msg", count == 0 ? "회원정보 삭제 실패" : "회원정보 삭제 성공" );
+		return new ResponseEntity(map, HttpStatus.OK);
+	}
+	
+	@PutMapping("/member/admin/update")
+	public ResponseEntity<String> updateMember(BoardMemberDTO dto){
+		System.out.println(dto);
+//		int count = 0;		
+		dto.setBoardMemberProfile(-1);
+		int count = memberService.updateMember(dto);		
+		Map<String, Object> map = new HashMap<String,Object>();
+		map.put("count", count);
+		map.put("msg", count == 0 ? "회원정보 수정 실패" : "회원정보 수정 성공" );
 		return new ResponseEntity(map, HttpStatus.OK);
 	}
  }
