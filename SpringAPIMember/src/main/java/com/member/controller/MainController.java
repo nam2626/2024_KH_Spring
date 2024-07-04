8240d716f58993da98619fded6b6d83dc107aac1
elzenaro <nam2626@naver.com>
2024-07-04 10:10:02 +0900

    업데이트

--- SpringAPIMember/src/main/java/com/member/controller/MainController.java
+++ SpringAPIMember/src/main/java/com/member/controller/MainController.java
@@ -1,12 +1,17 @@
 package com.member.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.member.dto.BoardMemberDTO;
+import com.member.dto.GradeDTO;
 import com.member.service.MemberService;
 
 @RestController
@@ -23,6 +28,17 @@ public class MainController {
 		return service.selectAllMember();
 	}
 	
+	@GetMapping("/grade/list")
+	public List<GradeDTO> selectAllGrade(){
+		return service.selectAllGrade();
+	}
+	
+	@PostMapping("/member/insert")
+	public Map<String, Object> insertMember(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("msg", "테스트 메세지");
+		return map;
+	}
 }
 
 
