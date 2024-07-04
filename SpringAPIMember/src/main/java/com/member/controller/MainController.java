a541d88eb0e42df5833114d2ade8b8a188ccc0a3
elzenaro <nam2626@naver.com>
2024-07-04 15:55:40 +0900

    회원정보 수정 - 컨트롤러

--- SpringAPIMember/src/main/java/com/member/controller/MainController.java
+++ SpringAPIMember/src/main/java/com/member/controller/MainController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,11 @@ public class MainController {
 			map.put("result", false);
 		}
 		return map;
+	}
+	
+	@GetMapping("/member/{id}")
+	public BoardMemberDTO selectMember(@PathVariable("id") String id) {
+		return service.selectMember(id);
 	}
 }
 
