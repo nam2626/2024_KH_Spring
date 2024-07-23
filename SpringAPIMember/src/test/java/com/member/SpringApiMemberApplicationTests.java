9a47603c72a1753a6e2ad86419040c1f1a9200f2
elzenaro <nam2626@naver.com>
2024-07-23 14:00:28 +0900

    jUnit Test

--- SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
+++ SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
@@ -1,13 +1,52 @@
 package com.member;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.member.dto.BoardMemberDTO;
+import com.member.service.MemberService;
 
 @SpringBootTest
 class SpringApiMemberApplicationTests {
-
+	
+	@Autowired
+	private MemberService service;
+	
 	@Test
-	void contextLoads() {
+	public void insertTest() {
+		BoardMemberDTO dto = new BoardMemberDTO();
+		dto.setBoardMemberId("TESTID");
+		dto.setBoardMemberName("테스트");
+		dto.setBoardMemberNick("테스트닉네임");
+		dto.setBoardMemberPasswd("123456");
+		dto.setBoardMemberGrade(1);
+		
+		int result = 0;
+		try {
+			result = service.insertMember(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(1, result,"데이터 추가 테스트 실패");
+		
 	}
 
+	
+	@Test
+	public void deleteTest() {
+		assertEquals(1, service.deleteMember("TESTID"),"데이터 삭제 테스트 실패");
+	}
 }
+
+
+
+
+
+
+
+
+
+
