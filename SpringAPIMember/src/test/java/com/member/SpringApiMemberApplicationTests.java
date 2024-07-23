c468230fd52e4afe8e3629e3f661eb1825a31b7d
elzenaro <nam2626@naver.com>
2024-07-23 14:52:53 +0900

    jUnit Test

--- SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
+++ SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
@@ -1,6 +1,7 @@
 package com.member;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,7 +11,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -38,8 +42,10 @@ class SpringApiMemberApplicationTests {
 	
 	@BeforeEach
 	//각 테스트 전 실행하는 메서드
-	public void testBefore() {
+	public void testBefore(TestInfo info) {
 		System.out.println("테스트 메서드 수행 전에 수행하는 메서드");
+		System.out.println(info.getTestMethod().get().getName() + " " 
+				+ info.getDisplayName());
 	}
 	
 	@AfterEach
@@ -79,6 +85,19 @@ class SpringApiMemberApplicationTests {
 		System.out.println("데이터 삭제 테스트 메서드");
 
 	}
+	
+	//회원 아이디 검색
+	//	데이터 3개를 검색한 결과
+	//	테스트할 데이터 "fo1311","px2959","sm9203"
+	//	assertNotNull(결과값, 실패했을때 메세지)
+	@DisplayName("검색 테스트")
+	@Order(3)
+	@ParameterizedTest
+	@ValueSource(strings = {"fo1311","px2959","sm9203","AAAAA"})
+	public void selectTest(String value) {
+		assertNotNull(service.selectMember(value),"검색 테스트 실패");
+	}
+	
 }
 
 
